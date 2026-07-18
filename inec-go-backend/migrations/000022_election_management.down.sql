4daedfee321e4522b1ff2b1f51da5018e8ac4fd2
munisp <155237317+munisp@users.noreply.github.com>
2026-07-18 14:10:57 -0400

    Renumber 000004_election_management to 000022 (duplicate version fix)

---
+++ inec-go-backend/migrations/000022_election_management.down.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS ems_workflows CASCADE;
+DROP TABLE IF EXISTS ems_workflow_phases CASCADE;
+DROP TABLE IF EXISTS election_lifecycle CASCADE;
+DROP TABLE IF EXISTS election_materials CASCADE;
+DROP TABLE IF EXISTS election_staff_assignments CASCADE;
+DROP TABLE IF EXISTS election_templates CASCADE;
+DROP TABLE IF EXISTS election_state_log CASCADE;
+DROP TABLE IF EXISTS election_archive CASCADE;
